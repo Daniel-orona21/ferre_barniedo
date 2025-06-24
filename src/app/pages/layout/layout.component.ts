93bb65fa24a762e7b8c3d7919039e95b614da80c
Alejandor Daniel López Orona <128829840+Daniel-orona21@users.noreply.github.com>
2025-06-24 11:27:25 -0600

    layout

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, CommonModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
+  isNavCollapsed = false;
+  showUserMenu = false;
+  notificationCount = 3;
+  currentRole: string = '';
 
+  constructor(
+    private router: Router, 
+    private eRef: ElementRef,
+  ) {}
+
+
+  toggleNav() {
+    this.isNavCollapsed = !this.isNavCollapsed;
+  }
+
+  toggleUserMenu() {
+    this.showUserMenu = !this.showUserMenu;
+  }
+
+  logout() {
+    this.router.navigate(['/']);
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (
+      this.showUserMenu &&
+      !this.eRef.nativeElement.querySelector('.user-menu')?.contains(target) &&
+      !this.eRef.nativeElement.querySelector('.arrow-user-menu')?.contains(target)
+    ) {
+      this.showUserMenu = false;
+    }
+  }
 }
