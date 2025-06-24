470c4131eed682915b095e94cd0e5d48a74217f2
Alejandor Daniel López Orona <128829840+Daniel-orona21@users.noreply.github.com>
2025-06-23 18:03:33 -0600

    login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginComponent, pathMatch: 'full' },
+    { path: 'app', component: LayoutComponent, children: [
+        { path: '', redirectTo: 'home', pathMatch: 'full' },
+        { path: 'home', component: HomeComponent }
+    ]}
+];
